dbc29197cafc1057a1890a596c6594cc0270ee47
Dylan Premo <maboose@yahoo.com>
2020-03-06 12:10:39 -0800

    Outline functions

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Warning: Executing Javascript from a string is an enormous security risk. It is far too easy for a bad actor to run arbitrary code when eval() is used. Never use eval()!
+
+// Basic functions
+const add = (num1, num2) => {
+
+}
+const subtract = (num1, num2) => {
+
+}
+const multiply = (num1, num2) => {
+
+}
+const divide = (num1, num2) => {
+  
+}
+
+// Complex functions
+const operate = (operator, num1, num2) => {
+
+}
