67da039ba5b3b33e292c6b0367f6e5071880e57e
Dylan Premo <maboose@yahoo.com>
2020-03-08 10:44:34 -0700

    Add number and symbol functions

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // Warning: Executing Javascript from a string is an enormous security risk. It is far too easy for a bad actor to run arbitrary code when eval() is used. Never use eval()!
 
+// Declare variables
+let history = document.getElementById('history');
+let total = document.getElementById('total');
+let num1;
+let num2;
+let operator;
+
 // Declare basic functions
 const add = (num1, num2) => {
   return num1 + num2;
@@ -14,14 +21,14 @@ const divide = (num1, num2) => {
   return num1 / num2;
 };
 
-// Test basic functions
-// console.log('2 + 2 = ' + add(2, 2));
-// console.log('2 - 2 = ' + subtract(2, 2));
-// console.log('2 * 2 = ' + multiply(2, 2));
-// console.log('2 / 2 = ' + divide(2, 2));
+/* // Test basic functions
+console.log('2 + 2 = ' + add(2, 2));
+console.log('2 - 2 = ' + subtract(2, 2));
+console.log('2 * 2 = ' + multiply(2, 2));
+console.log('2 / 2 = ' + divide(2, 2)); */
 
 // Declare complex functions
-const operate = (operator, num1, num2) => {
+const operate = (num1, operator, num2) => {
   if (operator === '+') {
     return add(num1, num2);
   } else if (operator === '-') {
@@ -34,41 +41,133 @@ const operate = (operator, num1, num2) => {
     alert('Please enter a valid operator. Valid operators include: + - * /');
   }
 };
+const equation = () => {
+  console.log('Testing');
+};
 
-// Test complex functions
-// console.log('2 + 2 = ' + operate('+', 2, 2));
-// console.log('2 - 2 = ' + operate('-', 2, 2));
-// console.log('2 * 2 = ' + operate('*', 2, 2));
-// console.log('2 / 2 = ' + operate('/', 2, 2));
+/* // Test complex functions
+console.log('2 + 2 = ' + operate('+', 2, 2));
+console.log('2 - 2 = ' + operate('-', 2, 2));
+console.log('2 * 2 = ' + operate('*', 2, 2));
+console.log('2 / 2 = ' + operate('/', 2, 2)); */
 
 // Declare number functions
 const zero = () => {
-  return 0;
+  if (total.innerHTML[0] !== '0' || total.innerHTML.indexOf('.') > 0) {
+    total.innerHTML += '0';
+  }
 };
 const one = () => {
-  return 1;
+  if (total.innerHTML[0] === '0' && total.innerHTML.indexOf('.') < 0) {
+    total.innerHTML = '1';
+  } else {
+    total.innerHTML += '1';
+  }
 };
 const two = () => {
-  return 2;
+  if (total.innerHTML[0] === '0' && total.innerHTML.indexOf('.') < 0) {
+    total.innerHTML = '2';
+  } else {
+    total.innerHTML += '2';
+  }
 };
 const three = () => {
-  return 3;
+  if (total.innerHTML[0] === '0' && total.innerHTML.indexOf('.') < 0) {
+    total.innerHTML = '3';
+  } else {
+    total.innerHTML += '3';
+  }
 };
 const four = () => {
-  return 4;
+  if (total.innerHTML[0] === '0' && total.innerHTML.indexOf('.') < 0) {
+    total.innerHTML = '4';
+  } else {
+    total.innerHTML += '4';
+  }
 };
 const five = () => {
-  return 5;
+  if (total.innerHTML[0] === '0' && total.innerHTML.indexOf('.') < 0) {
+    total.innerHTML = '5';
+  } else {
+    total.innerHTML += '5';
+  }
 };
 const six = () => {
-  return 6;
+  if (total.innerHTML[0] === '0' && total.innerHTML.indexOf('.') < 0) {
+    total.innerHTML = '6';
+  } else {
+    total.innerHTML += '6';
+  }
 };
 const seven = () => {
-  return 7;
+  if (total.innerHTML[0] === '0' && total.innerHTML.indexOf('.') < 0) {
+    total.innerHTML = '7';
+  } else {
+    total.innerHTML += '7';
+  }
 };
 const eight = () => {
-  return 8;
+  if (total.innerHTML[0] === '0' && total.innerHTML.indexOf('.') < 0) {
+    total.innerHTML = '8';
+  } else {
+    total.innerHTML += '8';
+  }
 };
 const nine = () => {
-  return 9;
+  if (total.innerHTML[0] === '0' && total.innerHTML.indexOf('.') < 0) {
+    total.innerHTML = '9';
+  } else {
+    total.innerHTML += '9';
+  }
+};
+
+/* // Test number functions
+console.log('0 = ' + zero());
+console.log('1 = ' + one());
+console.log('2 = ' + two());
+console.log('3 = ' + three());
+console.log('4 = ' + four());
+console.log('5 = ' + five());
+console.log('6 = ' + six());
+console.log('7 = ' + seven());
+console.log('8 = ' + eight());
+console.log('9 = ' + nine()); */
+
+//Declare symbol functions
+const decimal = () => {
+  if (total.innerHTML.indexOf('.') < 0) {
+    if (total.innerHTML.length < 1) {
+      total.innerHTML += '0.';
+    } else {
+      total.innerHTML += '.';
+    }
+  }
+};
+const addition = () => {
+  if (!isNaN(parseInt(total.innerHTML[total.innerHTML.length - 1]))) {
+    total.innerHTML += ' + ';
+  }
+};
+const subtraction = () => {
+  if (!isNaN(parseInt(total.innerHTML[total.innerHTML.length - 1]))) {
+    total.innerHTML += ' - ';
+  }
+};
+const multiplication = () => {
+  if (!isNaN(parseInt(total.innerHTML[total.innerHTML.length - 1]))) {
+    total.innerHTML += ' * ';
+  }
+};
+const division = () => {
+  if (!isNaN(parseInt(total.innerHTML[total.innerHTML.length - 1]))) {
+    total.innerHTML += ' / ';
+  }
+};
+
+// Declare top row functions
+const clearTotal = () => {
+  total.innerHTML = '';
+};
+const backspaceTotal = () => {
+  total.innerHTML = total.innerHTML.substring(0, total.innerHTML.length - 1);
 };
